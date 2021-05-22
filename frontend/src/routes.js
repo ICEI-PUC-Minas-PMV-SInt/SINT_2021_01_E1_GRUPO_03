354c412dc6242032e4702dc492d3f1aa460bae7d
Juan Marques <juan.marques@docshifter.com>
2021-05-22 22:43:22 +0200

    Fixing routes

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -1,44 +1,89 @@
 //Const das paginas
-const login = "./components/login/login.html";
-const profile = "./components/profile/profile.html";
-const header = "./components/header/header.html";
-const menuNav = "./components/menu-nav/menu-nav.html";
+const login = './components/login/login.html';
+const profile = './components/profile/profile.html';
+const header = './components/header/header.html';
+const menuNav = './components/menu-nav/menu-nav.html';
+const feed = './src/components/feed/feed.html';
 
 /**
  * Carrega pagina do Header.
  */
 async function loadHeader() {
-  const contentDiv = document.getElementById("header");
-  contentDiv.innerHTML = fetchHtmlAsText(header);
+  const headerDiv = document.getElementById('header');
+  headerDiv.innerHTML = await fetchHtmlAsText(header);
 }
 
 /**
  * Carrega pagina do menu nav.
  */
 async function loadMenuNav() {
-  const contentDiv = document.getElementById("menu_nav");
-  contentDiv.innerHTML = fetchHtmlAsText(menuNav);
+  const menuNavDiv = document.getElementById('menu_nav');
+  menuNavDiv.innerHTML = await fetchHtmlAsText(menuNav);
 }
 
 /**
  * Carrega pagina de login.
  */
 async function loadLogin() {
-  const contentDiv = document.getElementById("content");
-  contentDiv.innerHTML = fetchHtmlAsText(login);
+  const contentDiv = document.getElementById('content');
+  contentDiv.innerHTML = await fetchHtmlAsText(login);
 }
 
 /**
  * Carrega pagina de perfil.
  */
 async function loadProfile() {
-  const contentDiv = document.getElementById("content");
-  contentDiv.innerHTML = fetchHtmlAsText(profile);
+  const contentDiv = document.getElementById('content');
+  contentDiv.innerHTML = await fetchHtmlAsText(profile);
+}
+
+/**
+ * Carrega conteúdo do feed dinâmico com base no menu selecionado
+ */
+async function loadFeed(menuSelecionado) {
+  
+  const contentDiv = document.getElementById('content');
+
+  // Se nenhum menu tiver selecionado ignoramos
+  if (menuSelecionado !== undefined) {
+
+    const activeClassName = 'active_menu_nav';
+    const aparadorMenuNav = 'aparador_menu_nav';
+
+    menuSelecionado.classList.toggle(activeClassName);
+
+    const menuNavUl = document.getElementById('menu-nav-ul');
+
+    for (let i = 0; i < menuNavUl.children.length; i++) {
+      const menuNavLi = menuNavUl.children[i];
+
+      for (let i = 0; i < menuNavLi.children.length; i++) {
+        const menuNavLiChild = menuNavLi.children[i];
+
+        if (menuSelecionado !== menuNavLiChild && menuNavLiChild.classList.contains(activeClassName)) {
+          menuNavLiChild.classList.remove(activeClassName)
+        }
+
+        for (let i = 0; i < menuNavLiChild.children.length; i++) {
+          const menuNavAChild = menuNavLiChild.children[i];
+
+
+          if (menuSelecionado.children[0] !== menuNavAChild && menuNavAChild.classList.contains(aparadorMenuNav)) {
+            menuNavAChild.classList.remove(aparadorMenuNav)
+          }
+          else if (menuSelecionado.children[0] === menuNavAChild) {
+            menuNavAChild.classList.add(aparadorMenuNav)
+          }
+        }
+      }
+    }
+  }
+  contentDiv.innerHTML = await fetchHtmlAsText(feed);
 }
 
 /**
  * @param {String} url - Caminho da pagina que deve ser carregada
- * @return {String} Retorna a pagina como string
+ * @returns {Promise<string>} Retorna a pagina como string
  */
 async function fetchHtmlAsText(url) {
   return await (await fetch(url)).text();
